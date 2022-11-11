5e9c35ab9bc23cae2dd39e6fa6a4effe0e5b036d
Martín Calderón <martincalderon92@outlook.com>
2022-11-11 17:25:11 -0300

    Add Head to Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,15 @@
+import Head from "next/head";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Head>
+        <title>To Do App with Login</title>
+        <meta name="description" content="A simple to do app with a login" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
 };
 
 export default Home;
