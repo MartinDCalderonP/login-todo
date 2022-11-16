3fa7f138ff6c563c3274d5080b13dc631f6fda88
Martín Calderón <martincalderon92@outlook.com>
2022-11-16 15:44:20 -0300

    Add display block and max widht to default error message

--- components/ErrorMessage/styles.tsx
+++ components/ErrorMessage/styles.tsx
@@ -1,8 +1,10 @@
 import styled from "@emotion/styled";
 
 export const DefaultErrorMessage = styled("span")<{ textAlign?: string }>`
+  display: block;
   color: red;
   font-size: 0.75rem;
   margin-top: 0.25rem;
   text-align: ${({ textAlign }) => textAlign};
+  max-width: 100%;
 `;
